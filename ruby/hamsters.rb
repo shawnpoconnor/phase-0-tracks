cd5da32c9a3c8a5e0fdfa4cd6a2351a5f0c222ed
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-11 18:49:11 -0400

    deals with empty feild in age and adds print to console

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -10,7 +10,21 @@ def get_hamster_info
 
   puts "how old is yo hamster?"
   age = gets.chomp
+  if age.empty?
+    age = nil
+  else
+    age = age.to_i
+  end
 
   puts "Is your hamster good for adoption?"
   good_for_adoption = gets.chomp
+
+  puts "Hamster's Name: #{name}"
+  puts "Hamster's volume levle #{volume.to_i}"
+  puts "Fur color: #{fur_color}"
+  puts "Estamated hamster age: #{age}"
+  puts "Good for adoption?: #{good_for_adoption}"
+
 end
+
+get_hamster_info
