fd7a2897db4d32d212dd06d9493aa00b6b0f72dd
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-11 18:17:59 -0400

    adds method to get info on hamsters

---
+++ ruby/hamsters.rb
@@ -0,0 +1,16 @@
+def get_hamster_info
+  puts "what is this hamster's name?"
+  name = gets.chomp
+
+  puts "on a scale of 1-10 how loud is this hamster?"
+  volume = gets.chomp
+
+  puts "what is the hamster's fur color?"
+  fur_color = gets.chomp
+
+  puts "how old is yo hamster?"
+  age = gets.chomp
+
+  puts "Is your hamster good for adoption?"
+  good_for_adoption = gets.chomp
+end
