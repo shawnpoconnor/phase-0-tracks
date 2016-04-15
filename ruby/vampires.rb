d4fdda9cbeccff1e2688fa77985372b39b7ae3c9
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-15 16:39:12 -0400

    Creates a question form in vampires.rb

---
+++ ruby/vampires.rb
@@ -0,0 +1,12 @@
+def get_vamp_info
+  puts "What is your name"
+  name = gets.chomp
+
+  puts "How old are you? What year were you born?"
+  age_and_YOB = gets.chomp
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  eats_garlic = gets.chomp
+
+  puts "would you like to enroll in the company's health insurance?"
+  health_insureance = gets.chomp
