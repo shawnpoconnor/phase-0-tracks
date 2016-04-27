5d38a41808d904609f1e1a1fbf545ea59b0d21d4
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-27 18:46:14 -0400

    creates Puppy class and defines 2 methods

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -1,11 +1,23 @@
-def calculate (integer1 operator integer2)
-  case operator
-  when operator == '+'
-    then integer1 + integer2
-  when operator == '-'
-    then integer1 - integer2
-  when operator == '*'
-    then integer1 * integer2
-  when operator == '/'
-    then integer1 / integer2
+def calculate (integer1, operator, integer2)
+  if operator == '+'
+    return integer1 + integer2
+  elsif operator == '-'
+    return integer1 - integer2
+  elsif operator == '*'
+    return integer1 * integer2
+  elsif operator == '/'
+    return integer1 / integer2
+  end
 end
+
+def interface
+  puts "please enter a calculation(ex. '5+4')"
+  calculation = gets.chomp
+  calculation = calculation.split(//)
+  if calculation.include?(" ")
+    calculation = calculation.delete(" ")
+  end
+  calculate(calculation[0].to_i, calculation[1], calculation[2].to_i)
+end
+
+interface
