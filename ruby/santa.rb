424ffacf1899b25b00209af7f437f98aa0b64588
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-27 20:21:27 -0400

    adds instance variables, and interface method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,21 @@ class Santa
     puts "That was a good #{cookie_type}"
   end
 
-  def initialize
+  def initialize(name gender)
     puts "Initalizing Santa"
+    @name = name
+    @gender = gender
+    @reindeer_ranking = %w(Rudolph Dasher Dancer Prancer Vixen Comet Cupid Donner Blitzen)
+    @age = 0
   end
 end
+
+def interface
+  puts "What is your santas name?"
+  name = gets.chomp
+  puts "what is your santas gender?"
+  gender = gets.chomp
+end
+
+santas = []
+puts "how many santas would you like to make?"
