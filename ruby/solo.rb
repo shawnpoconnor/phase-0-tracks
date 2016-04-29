1b29f856dc7462a0c554e45c8b39a2d03c6a03e2
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-29 19:10:59 -0400

    creates list file and TodoList class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,14 +1,38 @@
+=begin
+Rick class
+========
+Attributes
+------------
+hairstyle
+family
+diminsion
+time
+age
+gender
+race
+
+Methods
+------------
+initialize(sets attributes and prints initialize statement)
+invent
+interdemsional travle
+time travle
+find location
+=end
 class Ricks
   attr_accessor :hairstyle, :family
-  attr_reader :diminsion, :time, :age, :gender
+  attr_reader :diminsion, :time, :gender, :race, :name
 
-  def initialize (hairstyle, gender, location)
+  def initialize (diminsion, name = "Rick Sanchez", hairstyle = "Standard Rick Style", gender = "Male", race = "Human", age = 60)
+    @name = name
     @hairstyle = hairstyle
     @gender = gender
-    @diminsion = location
+    @diminsion = diminsion
     @time = "Present-Time"
-    @age = 60
+    @age = age
     @family = ["Beth", "Jerry", "Summer", "Morty"]
+    puts "Wubba-Lubba-Dubdub"
+  end
 
   def invent(invention)
     "Look at *belch* this #{invention.to_s}. It's *burp* It's gonna change the game Morty."
@@ -21,4 +45,18 @@ class Ricks
   def time_travle(time)
     @time = time
   end
+
+  def location
+    puts "This rick is in the#{diminsion} in year #{time}"
+  end
+end
+
+rick_c137 = Ricks.new("C-137")
+rick_c137.time_travle(1960)
+rick_c137.interdeminsional_travle("J19ζ7")
+rick_c137.location
+puts rick_c137.name
+
+def interface
+  puts "How many Ricks would you like to make?"
 end
