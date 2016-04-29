44a6b41195439d04f88c1d1794e10a6d06eebe20
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-29 19:27:40 -0400

    writes methods to pass first 2 texts

--- ruby/list/list.rb
+++ ruby/list/list.rb
@@ -1,2 +1,14 @@
 class TodoList
+  attr_reader :chores
+  def initialize(chores)
+    @chores = chores
+  end
+
+  def get_items
+    @chores
+  end
+
+  def add_item(item)
+    @chores << item
+  end
 end
