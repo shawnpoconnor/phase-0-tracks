19f0ec0847a3fe9bea28e8b7a4cfeac1113c01a2
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-29 16:46:00 -0400

    psudocodes puppy class and then actually creates a puppy class then creates a few instances of Puppiesgit add puppy.rb! SO CUTE

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+#Puppy Class
+
+# ATTRIBUTES:
+# -----------------------
+# Cuteness levle
+# Energy levle
+# Size
+# Fur length
+
+# BEHAVIOR
+# ------------------------
+# Jump
+# Fetch
+# Play Dead
+# Shake
+
+class Puppy
+end
+
+karl = Puppy.new
+joe = Puppy.new
+mrs.cuteie = Puppy.new
