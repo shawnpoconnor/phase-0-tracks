8aa23e6646eaa46d9a39255a6555bcda911ff31a
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-29 17:17:12 -0400

    add some awesome rick methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,15 +1,24 @@
 class Ricks
-  attr_accessor :hairstyle, :gender, :family, :location
+  attr_accessor :hairstyle, :family
+  attr_reader :diminsion, :time, :age, :gender
 
   def initialize (hairstyle, gender, location)
     @hairstyle = hairstyle
     @gender = gender
-    @location = location
+    @diminsion = location
+    @time = "Present-Time"
     @age = 60
     @family = ["Beth", "Jerry", "Summer", "Morty"]
 
-  def invent
-    "Look at *belch* this. It's *burp* It's gonna change the game Morty."
+  def invent(invention)
+    "Look at *belch* this #{invention.to_s}. It's *burp* It's gonna change the game Morty."
   end
 
+  def interdeminsional_travle(diminsion)
+    @diminsion = diminsion
+  end
+
+  def time_travle(time)
+    @time = time
+  end
 end
