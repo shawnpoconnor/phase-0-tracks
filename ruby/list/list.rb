a9ca6a86fd007591a1efb78f11d9706b02db8671
Shawn O'Connor <bartandfosto@gmail.com>
2016-04-29 19:40:37 -0400

    adds methods to make last two tests pass

--- ruby/list/list.rb
+++ ruby/list/list.rb
@@ -11,4 +11,19 @@ class TodoList
   def add_item(item)
     @chores << item
   end
+
+  def delete_item(item_to_delete)
+    i = 0
+    while i < @chores.length
+      if @chores[i] == item_to_delete
+          @chores.delete_at(i)
+          break
+        end
+        i+=1
+      end
+    end
+
+    def get_item(item_index)
+      @chores[item_index]
+    end
 end
