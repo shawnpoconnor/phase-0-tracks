0c88b1baf12b77db240dabb52c032be0ccb68d88
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-02 19:02:34 -0400

    makes function to reverse word

--- js/explore.js
+++ js/explore.js
@@ -2,3 +2,14 @@
 // Loop through string from last index to first
 //   -add each letter in the loop to empty string
 //  print backwards string to console
+var string = "hello";
+
+function reverseWord(word) {
+  var backwardsString = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    backwardsString = backwardsString + word[i];
+  };
+  console.log(backwardsString)
+}
+
+reverseWord(string);
