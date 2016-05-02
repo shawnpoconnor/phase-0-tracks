6269a919c68f4358bec0a5bf84fac2a8872ed198
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-01 23:36:13 -0400

    adds new tests for dancer card and makes them pass

--- ruby/dancer/dancer.rb
+++ ruby/dancer/dancer.rb
@@ -1,9 +1,29 @@
 class Dancer
-  attr_accessor :name, :age
-
-  def initialize(name, age)
+  attr_accessor :name, :age, :card
+  def initialize(name, age, card = [])
     puts "Initializeing Dancer"
     @name = name
     @age = age.to_i
+    @card = card
+  end
+
+  def pirouette
+    return "*twirls*"
+  end
+
+  def bow
+   return "*bows*"
+  end
+
+  def queue_dance_with(name)
+    card << name
+  end
+
+  def begin_next_dance
+    now_dancing = card.delete_at(0)
+    return "Now dancing with #{now_dancing}."
+
   end
 end
+
+a = Dancer.new("Jane", 22)
