9cbc9c029ad5bcef2282e94a63cd123c77483daf
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-03 17:31:59 -0400

    creates 2 functions to add data to arrays

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+var warmColors = ["red", "yellow", "burnt sienna", "Orange"];
+var coolHorses = ['Peaches', 'Gary', 'Colonel', 'Steve-dave'];
+
+function addColor(color) {
+  warmColors.push(color);
+  console.log(warmColors);
+};
+
+function addHorse (horse) {
+  coolHorses.push(horse);
+  console.log(coolHorses);
+};
+
+addColor("blurple");
+addHorse("Trevor");
