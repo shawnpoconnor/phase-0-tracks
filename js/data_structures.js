8c63c3373e55bec617fe79d9be9d76796a42e7df
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-03 17:59:39 -0400

    adds loop that makes object out of two arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,6 +1,6 @@
 var warmColors = ["red", "yellow", "burnt sienna", "Orange"];
 var coolHorses = ['Peaches', 'Gary', 'Colonel', 'Steve-dave'];
-
+var colorfulHorses = {};
 function addColor(color) {
   warmColors.push(color);
   console.log(warmColors);
@@ -13,3 +13,14 @@ function addHorse (horse) {
 
 addColor("blurple");
 addHorse("Trevor");
+
+for (var i = 0; i < coolHorses.length; i++) {
+  colorfulHorses[coolHorses[i]] = warmColors[i];
+}
+console.log(colorfulHorses);
+
+// function makeObject (object, colors, horses)
+//   for (var i = 0; i < horses.length; i++) {
+//     object[horses[i]] = colors[i];
+//     console.log(object)
+//   };
