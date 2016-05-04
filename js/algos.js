ed80bc19595cf80a3d775e5c9f89df11e64bd780
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-04 17:21:46 -0400

    Begins Psudocode for release 2

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,7 @@
-// **PSUDOCODE**
-// -- Method finds longest word or phrase in grouping of strings
+//                              **FUNCTION LIBRARY**
+// ----------------------------------------------------------------------------------
+// // **PSUDOCODE**
+// -- Function finds longest word or phrase in grouping of strings
 // 1.Set longestString variable to empty string
 // 2. Loop through the array of words/phrases
 //   * if the length of word/phrase is longer than longestString
@@ -12,6 +14,15 @@ function findLongestString(wordsPhrases) {
       longestString = wordsPhrases[i];
     }
   }
+  console.log(longestString)
 }
 
+// --------------------------------------------------------------------------------
+**PSUDOCODE**
+-- Function checks one object against another to see if there are matching key-value pairs, and returns an associated boolian
+      1.
+
+// =============================================
+//                                **DRIVER/TESTING**
 findLongestString(["long phrase","longest phrase","longer phrase"])
+
