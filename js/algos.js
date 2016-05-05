8964f68364c102a602dc0a5c591d7cdf147e06fd
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-04 20:16:11 -0400

    Adds psudocode for matching key-values

--- js/algos.js
+++ js/algos.js
@@ -20,7 +20,13 @@ function findLongestString(wordsPhrases) {
 // --------------------------------------------------------------------------------
 **PSUDOCODE**
 -- Function checks one object against another to see if there are matching key-value pairs, and returns an associated boolian
-      1.
+      1. Set matchingValue variable to false
+      2. loop thorugh object1
+          *loop through object2 checking for matching keys
+            +if object1's key matches object2's key
+              = if match object1 value with object2 value
+                -set matchingValue to true
+      3. return matchingValue
 
 // =============================================
 //                                **DRIVER/TESTING**
