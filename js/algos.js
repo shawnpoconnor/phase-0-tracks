41ef28c17b26af183f17874da2ad08a9ceb15112
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-08 17:30:42 -0400

    adds psudocode for last release and generate random word function

--- js/algos.js
+++ js/algos.js
@@ -1,5 +1,5 @@
 //                              **FUNCTION LIBRARY**
-// ----------------------------------------------------------------------------------
+// ===============================================
 // // **PSUDOCODE**
 // -- Function finds longest word or phrase in grouping of strings
 // 1.Set longestString variable to empty string
@@ -20,6 +20,7 @@ function findLongestString(wordsPhrases) {
 // --------------------------------------------------------------------------------
 // **PSUDOCODE**
 // -- Function checks one object against another to see if there are matching key-value pairs, and returns an associated boolian
+// Double Loop method
 //       1. Set matchingValue variable to false
 //       2. loop thorugh object1
 //           *loop through object2 checking for matching keys
@@ -28,17 +29,66 @@ function findLongestString(wordsPhrases) {
 //                 -set matchingValue to true
 //       3. return matchingValue
 
-var isSame = false;
+// Single Loop method
+// 1. set matchingValue variable to false
+// 2. Loop through object one looking at each key
+//   *if object1 indexed at key is equal to object 2 indexed at key
+//     +set matchingValue to true
+// 3.print matchingValue to console
+
 function keyValueSame(object1, object2) {
+  var isSame = false;
   for (var obj1Key in object1) {
-    for (var obj2Key in object2) {
-    //   if(object1[obj1Key] === object2[obj2Key]) {
-    //     isSame = true;
+    if (object2.hasOwnProperty(obj1Key)) {
+      if (object1[obj1Key] === object2[obj1Key]) {
+        isSame = true;
       };
     };
+  };
 console.log(isSame);
 };
+
+// --------------------------------------------------------------------------------
+// **PSUDOCODE**
+// -- Function populateRandWords that is (n) indexes long of random words of varing length.
+//       1. set randWords variable to empty string
+//       2. run a for loop n times
+//         *randWords gets outcome of generateWord function
+//       3. console.log(randWords)
+
+// -- Function generateRandWord
+//       1. Set randNum variable to randomly generating number between 3-10
+//       2. Set possibleLetters variable to a-z
+//       3. Set newWord variable to empty string
+//       4. run for loop randNum amout of times
+//         *newWord gets random letter from possibleLetters
+//       5.return newWord
+
+function generateRandWord() {
+  var randNum = (Math.floor((Math.random() * 10) + 3));
+  var possibleLetters = "abcdefghijklmnopqrstuvwxyz";
+  var newWord = "";
+  for( var i=0; i < randNum; i++ ); {
+    text += possibleLetters.charAt(Math.floor(Math.random() * possible.length));
+   };
+  return newWord;
+};
+
 
 // =============================================
 //                                **DRIVER/TESTING**
 // findLongestString(["long phrase","longest phrase","longer phrase"])
+
+// var a = {
+//     "age" : "55",
+//     "height" : "6'",
+//     "weight" : "200"
+// };
+
+// var b = {
+//   "age" : "23",
+//   "height" : "5'",
+//   "weight" : "165"
+//   }
+
+// keyValueSame(b, a)
