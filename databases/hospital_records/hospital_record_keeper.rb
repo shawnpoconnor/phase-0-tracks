e8f436d6b72572e47da18bcea259391ee5ebe02b
Shawn O'Connor <bartandfosto@gmail.com>
2016-05-16 02:56:01 -0400

    adds some methods to populate tables

--- databases/hospital_records/hospital_record_keeper.rb
+++ databases/hospital_records/hospital_record_keeper.rb
@@ -3,7 +3,7 @@ require 'sqlite3'
 require 'faker'
 
 # Make hospital database file
-db = SQLite3::Database.new("patient_info.db")
+db = SQLite3::Database.new("patient_doctor_info.db")
 db.results_as_hash = true
 
 # declare doctors table execution variable.
@@ -11,7 +11,7 @@ create_doctor_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS doctors(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    number_of_patients int
+    number_of_patients INT
   )
 SQL
 
@@ -22,10 +22,28 @@ create_patients_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS patients(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    age INT,
     doctor_id INT,
-    illness_id INT,
-    FOREIGN KEY (doctor_id) REFERENCES  doctors(id),
-    FOREIGN KEY (illness_id) REFERENCES  illness(id)
+    illness VARCHAR(255),
+    FOREIGN KEY (doctor_id) REFERENCES  doctors(id)
   )
 SQL
+
+# create a patients table (if it's not there already)
+db.execute(create_patients_table_cmd)
+
+# create a doctor table (if it's not there already)
+db.execute(create_doctor_table_cmd)
+
+def add_patient(db, name, illness)
+  db.execute("INSERT INTO patients (name, illness) VALUES (?, ?)", [name, illness])
+end
+
+def add_doctor(db, name)
+  db.execute("INSERT INTO doctors (name, number_of_patients) VALUES (?, 0)", [name])
+end
+
+def generate_random_patients(number_of_patients)
+illness = ["Flu", "Cold", "Ricktts", "TB", "Inflamation", "Fracture", "Cut", "Sprain"]
+number_of_patients.times do
+  add_patient(db, Faker::Name.name, illness.sample)
+end
